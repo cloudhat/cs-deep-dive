98b58cb2e56e86d3a96f128e29ec54e01539e6d3
hyejin30 <tyui3185@gmail.com>
2022-07-30 09:41:47 +0900

    Fix

--- data_structure_and_algorithm/kim_hyejin/week1.js
+++ data_structure_and_algorithm/kim_hyejin/week1.js
@@ -8,6 +8,6 @@ for (let n = 2; 8 * n ** 2 < 64 * n * Math.sqrt(n); n++) {
 // 1.2-3
 // 100n제곱이 2n승보다 빠르게 실행되는 n의 최소값은 무엇입니까?
 
-for (let n = 2; 100 * n ** 2 < Math.pow(n, 2); n++) {
+for (let n = 2; 100 * n ** 2 > Math.pow(n, 2); n++) {
   console.log("beat value", n);
 }
