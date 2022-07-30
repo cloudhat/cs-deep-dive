9175b18a40f0e7314ea8f26e649f7bcb82000383
hyejin30 <tyui3185@gmail.com>
2022-07-30 09:40:41 +0900

    Fix

--- data_structure_and_algorithm/kim_hyejin/week1.js
+++ data_structure_and_algorithm/kim_hyejin/week1.js
@@ -1,13 +1,13 @@
 // 1.2-2
 // 삽입 정렬이 병합 정렬을 능가하는 n 값은 무엇입니까? (삽입 정렬이 더 빠를 때의 n값)
 
-for (let n = 2; 8 ** n < 64 * n * Math.sqrt(n); n++) {
+for (let n = 2; 8 * n ** 2 < 64 * n * Math.sqrt(n); n++) {
   console.log("beat value", n);
 }
 
 // 1.2-3
 // 100n제곱이 2n승보다 빠르게 실행되는 n의 최소값은 무엇입니까?
 
-for (let n = 2; 100 ** n < Math.pow(n, 2); n++) {
+for (let n = 2; 100 * n ** 2 < Math.pow(n, 2); n++) {
   console.log("beat value", n);
 }
