9fbbf61eaffe7beedbc11e9c69ce39ec59651f7a
kimeunji <monahk93@gmail.com>
2022-08-22 13:10:58 +0900

    fix : insertion sort

--- data_structure_and_algorithm/kej/insertion_sort.py
+++ data_structure_and_algorithm/kej/insertion_sort.py
@@ -5,7 +5,7 @@ from array import array
 def insertion_sort(numbers : array) -> array:
     key = 1
     for i in range(key, len(numbers)):
-        for j in range(i, 1, -1):
+        for j in range(i, 0, -1):
             if numbers[j] < numbers[j-1]:
                 numbers[j-1], numbers[j] = numbers[j], numbers[j-1]
                 continue
@@ -15,4 +15,4 @@ def insertion_sort(numbers : array) -> array:
                      
     return numbers
 
-print(insertion_sort([1,3,4,2,7,5,6]))
+print(insertion_sort([3,4,2,7,5,6,1]))
