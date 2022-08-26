0f2c460cab624c9a602a6cec7026ad67296a3e78
lesol <wingnawing@gmail.com>
2022-08-26 21:56:12 +0900

    week 4 heap

---
+++ data_structure_and_algorithm/solee/week4.py
@@ -0,0 +1,21 @@
+from heapq import heappush, heappop
+
+# 정렬 되지 않은 배열로부터 최대-힙을 만든다
+def build_max_heap():
+    print(1)
+
+# 그 루트의 서브 트리에서 힙 특성을 위반한 걸 한 가지 고친다.
+def max_heapify():
+    print(1)
+
+def insert():
+    print(1)
+
+def extract_max():
+    print(1)
+
+def heapsort():
+    print(1)
+
+def insert():
+    print(1)
