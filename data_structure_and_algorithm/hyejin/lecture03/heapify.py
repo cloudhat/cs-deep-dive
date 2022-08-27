b4ea93f5a784aa72adced9875148531310311a9d
hyejin30 <tyui3185@gmail.com>
2022-08-27 09:25:57 +0900

    HYEJIN : lecture03 - heapify

---
+++ data_structure_and_algorithm/hyejin/lecture03/heapify.py
@@ -0,0 +1,35 @@
+def swap(tree, index_1, index_2):
+    """완전 이진 트리의 노드 index_1과 노드 index_2의 위치를 바꿔준다"""
+    temp = tree[index_1]
+    tree[index_1] = tree[index_2]
+    tree[index_2] = temp
+
+
+def heapify(tree, index, tree_size):
+    """heapify 함수"""
+
+    # 왼쪽 자식 노드의 인덱스와 오른쪽 자식 노드의 인덱스를 계산
+    left_child_index = 2 * index
+    right_child_index = 2 * index + 1
+
+    # 코드를 쓰세요.
+    # 1. 일단 부모 노드의 값이 가장 크다고 설정
+    largest = index  
+
+    # 2. 왼쪽 자식 노드의 값과 비교
+    if 0 < left_child_index < tree_size and tree[largest] < tree[left_child_index]:
+        largest = left_child_index
+
+    # 3. 오른쪽 자식 노드의 값과 비교
+    if 0 < right_child_index < tree_size and tree[largest] < tree[right_child_index]:
+        largest = right_child_index
+
+    # 4. 최대값 vs 부모 노드 비교
+    if largest != index:
+        swap(tree, index, largest)
+        heapify(tree, largest, tree_size)
+        
+# 실행 코드
+tree = [None, 15, 5, 12, 14, 9, 10, 6, 2, 11, 1]  # heapify하려고 하는 완전 이진 트리
+heapify(tree, 2, len(tree))  # 노드 2에 heapify 호출
+print(tree) 
